9168585548dba27f11d1144bfb807dab5c449f3e
hababisha <haileamlakderbie1@gmail.com>
2024-11-01 16:34:23 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  
+  return (
+    <>
+     <div className='bg-black text-white h-screen flex justify-center items-center'>Hello world</div>
+    </>
+  )
+}
+
+export default App
